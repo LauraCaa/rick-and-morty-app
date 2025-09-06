b16ffd4204df2c06b473d647234b781b6a14ac09
Camila <cardenascastrolauracamila7@gmail.com>
2025-09-06 10:44:40 -0500

    feat: implement responsive mobile view for CharacterList

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet, useLocation } from "react-router-dom";
+import Header from "../ui/Header";
 import StarredCharacterList from "../characters/StarredCharacterList";
 import CharacterList from "../characters/CharacterList";
-import Header from "../ui/Header";
 
 export default function Layout() {
   const location = useLocation();
@@ -9,23 +9,22 @@ export default function Layout() {
 
   return (
     <div className="h-screen bg-white text-gray-800 flex flex-col md:flex-row">
-      <aside className={`hidden md:flex w-96 bg-[#fafafa] px-1 py-10 flex-shrink-0 overflow-y-auto flex-col`}>
+      <aside className="hidden md:flex w-96 bg-[#fafafa] px-1 py-10 flex-shrink-0 overflow-y-auto flex-col">
         <Header />
         <StarredCharacterList />
         <CharacterList />
       </aside>
 
-      <main className="flex-1 overflow-y-auto overflow-x-hidden py-6 p-4">
-        <div className="md:hidden flex flex-col">
-          {!isCharacterPage && (
-            <>
-              <Header isMobile={true} />
-              <StarredCharacterList />
-              <CharacterList />
-            </>
-          )}
+      <main className="flex-1 overflow-y-auto overflow-x-hidden py-6 px-1">
+        <div className={`md:hidden ${isCharacterPage ? 'hidden' : 'block'}`}>
+          <Header isMobile={true} />
+          <StarredCharacterList />
+          <CharacterList />
         </div>
-        <Outlet />
+        
+        <div className={`${isCharacterPage ? 'block' : 'hidden'} md:block h-full`}>
+          <Outlet />
+        </div>
       </main>
     </div>
   );
